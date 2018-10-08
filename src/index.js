c4b3332df993b6f7e52296fe1ed019f72d1de41d
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-08 10:59:01 -0700

    Adds cardlist.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+// React loads Webpack, so import is legal
 import React from 'react';
+
+// React can be used for different outputs
+// ReactDOM is for the web.
 import ReactDOM from 'react-dom';
+
+// This import allows you to import style for each component
+// .css is required
 import './index.css';
-import App from './App';
+
+// Import a convenience library
+import 'tachyons';
+
+// Import robots array
+import { robots } from './robots'
+
+// Import Card
+import Cardlist from './Cardlist';
+
+// Make app work faster offline
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Render the app
+ReactDOM.render(
+  <Cardlist robots={robots} />
+  ,document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/react_notes.md
@@ -0,0 +1,28 @@
+# React
+
+## Isn't mixing CSS and JS a bad?
+Usually yes.
+
+The philosophy with React is that the Component is king.  
+
+Since modern interaction couples CSS and JS anyway, simply acknowledge that fact, and redraw our separation of concern around a consistent piece: the component.
+
+## React Classes are Functions
+React Class
+```js
+class Hello extends Component {
+    render() {
+      return (
+          <h1>Hello {this.props.name}!</h1>
+      );
+    }
+}
+```
+Function
+```js
+const Hello = (props) => {
+    return (
+        <h1>Hello {props.name}!</h1>
+    );
+};
+```
