889cefd31cfb42408a182aa09f84cedf6c9a7764
Kevin Jackson <kevin@kevinjacksonart.com>
2018-10-08 14:57:51 -0700

    Adds bg and title style.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   render() {
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Cardlist robots={this.filteredRobots()} />
       </div>
